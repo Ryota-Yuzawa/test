c61e1434fb675a81a35b2ffa90e0d3aafd685209
Yuzawa <ham-and-egg@hotmail.co.jp>
2023-12-12 22:47:33 +0900

    ソート機能の改善・saleControllerにDBトランザクション追加
    検索結果を保持した状態でソート可能。

--- app/Http/Controllers/API/SaleController.php
+++ app/Http/Controllers/API/SaleController.php
@@ -10,35 +10,53 @@ use App\Http\Controllers\Controller;
 
 class SaleController extends Controller
 {
-    public function purchase (Request $request) {
+    public function purchase(Request $request)
+    {
+        // トランザクションを開始
+        DB::beginTransaction();
 
-        // リクエストから必要なデータを取得する
-        $product_id = $request->input('product_id'); 
-        $quantity = $request->input('quantity', 1);
+        try {
+            // リクエストから必要なデータを取得する
+            $product_id = $request->input('product_id');
+            $quantity = $request->input('quantity', 1);
 
-        // データベースから対象の商品を検索・取得
-        $product = Product::find($product_id);
+            // データベースから対象の商品を検索・取得
+            $product = Product::find($product_id);
 
-        // 商品が存在しない、または在庫が不足している場合のバリデーションを行う
-        if (!$product) {
-            return response()->json(['message' => '商品が存在しません'], 404);
+            // 商品が存在しない場合のバリデーションを行う
+            if (!$product) {
+                return response()->json(['message' => '商品が存在しません'], 404);
+            }
+
+            // 在庫が不足している場合のバリデーションを行う
+            if ($product->stock < $quantity) {
+                return response()->json(['message' => '商品が在庫不足です'], 400);
+            }
+
+            // 在庫を減少させる
+            $product->stock -= $quantity;
+            $product->save();
+
+            // Salesテーブルに商品IDと購入日時を記録する
+            $sale = new Sale([
+                'product_id' => $product_id,
+            ]);
+
+            $sale->save();
+
+            // トランザクションをコミット
+            DB::commit();
+
+            // レスポンスを返す
+            return response()->json(['message' => '購入成功']);
+        } catch (\Exception $e) {
+            // エラーが発生した場合、トランザクションをロールバック
+            DB::rollback();
+
+            // エラーをログに記録するなどの処理を追加できます
+
+            // エラーレスポンスを返す
+            return response()->json(['message' => '購入処理に失敗しました'], 500);
         }
-        if ($product->stock < $quantity) {
-            return response()->json(['message' => '商品が在庫不足です'], 400);
-        }
-
-        // 在庫を減少させる
-        $product->stock -= $quantity;
-        $product->save();
-
-        // Salesテーブルに商品IDと購入日時を記録する
-        $sale = new Sale([
-        'product_id' => $product_id,
-        ]);
-
-        $sale->save();
-
-        // レスポンスを返す
-        return response()->json(['message' => '購入成功']);
     }
 }
--- public/js/sample.js
+++ public/js/sample.js
@@ -1,8 +1,11 @@
 // 外部ファイル: custom-script.js
 
 $(document).ready(function() {
+    // 検索結果を保持する変数
+    let searchResultData = null;
+
     // CSRFトークンの設定
-    var csrfToken = $('meta[name="csrf-token"]').attr('content');
+    let csrfToken = $('meta[name="csrf-token"]').attr('content');
     if (csrfToken) {
         $.ajaxSetup({
             headers: {
@@ -11,22 +14,37 @@ $(document).ready(function() {
         });
     }
 
+    // テーブルソーターの初期化
+    $('#table').tablesorter();
+
     // 検索フォームの送信処理
     $('#searchForm').submit(function(e) {
         e.preventDefault();
-        var searchFormAction = $(this).attr('action'); // Formのaction属性からURLを取得
+        let searchFormAction = $(this).attr('action'); // Formのaction属性からURLを取得
         $.ajax({
             type: "GET",
             url: searchFormAction, // Formのaction属性を使用
             data: $(this).serialize(),
             success: function(data) {
+                // 検索結果を保存
+                searchResultData = data;
                 $('#productTable').find('tbody').html($(data).find('tbody').html());
+                $('#table').tablesorter();
             }
         });
     });
 
-    // テーブルソーターの初期化
-    $('#table').tablesorter();
+    // ソートボタンのクリックイベント
+    $('#table').click(function() {
+        if (searchResultData) {
+            // 検索結果がある場合はそれを使ってテーブルを更新
+            $('#productTable').find('tbody').html($(searchResultData).find('tbody').html());
+            $('#table').trigger('update');
+        } else {
+            // 検索結果がない場合は通常のソート処理
+            $('#table').trigger('sorton', [0, 0]); // 例: 列0で昇順ソート
+        }
+    });
 
     // 削除ボタンのクリックイベント
     $(document).on('click', '.delete-button', function() {
@@ -54,3 +72,4 @@ $(document).ready(function() {
         }
     });
 });
+
